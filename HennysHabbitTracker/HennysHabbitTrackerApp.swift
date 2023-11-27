cd71c4a21707b8070cb7726f7ad673172a530a19
Henny2 <henrieke.baunack@web.de>
2023-11-26 17:40:20 -0800

    Initial Commit

---
+++ HennysHabbitTracker/HennysHabbitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HennysHabbitTrackerApp.swift
+//  HennysHabbitTracker
+//
+//  Created by Henrieke Baunack on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct HennysHabbitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
