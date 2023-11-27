a976bcf3d11d2ead448598570f2edb106a86b2f6
Henny2 <henrieke.baunack@web.de>
2023-11-26 19:35:00 -0800

    adding data structure for habbits

--- HennysHabbitTracker/ContentView.swift
+++ HennysHabbitTracker/ContentView.swift
@@ -8,18 +8,24 @@
 import SwiftUI
 import Observation
 
-enum Frequency: Hashable {
+enum Frequency {
     case weekly, daily
 }
 
+// this is basically the data structure for storing habbit info, thus it's a struct
 struct Habbit: Hashable {
     var name: String
     var description: String
-//    var frequency: Frequency
+    var frequency: Frequency
 }
 
+// we want to keep track of the list of habbits and pass that around, so we track that with @Observable
 @Observable class Habbits {
     var habbitList: [Habbit]
+    
+    // we can change the above to the following to get rid of the init() method but for that we need the add view first
+    // var habbitList = [Habbit]()
+    
     
     init(habbitList: [Habbit]) {
         self.habbitList = habbitList
@@ -27,15 +33,15 @@ struct Habbit: Hashable {
 }
 
 struct ContentView: View {
-    @State var userHabbits = Habbits(habbitList: [Habbit(name: "Walk the dog", description: "Go on a walk with the dog every day"), Habbit(name: "Do Laundry", description: "Do the dirty laundry")])
-    let habbits = [Habbit(name: "Walk the dog", description: "Go on a walk with the dog every day"), Habbit(name: "Do Laundry", description: "Do the dirty laundry")]
+    @State var userHabbits = Habbits(habbitList: [Habbit(name: "Walk the dog", description: "Go on a walk with the dog every day", frequency: .daily), Habbit(name: "Do Laundry", description: "Do the dirty laundry", frequency: .weekly), Habbit(name: "Do Laundry", description: "Do the dirty laundry", frequency: .daily)])
+//    let habbits = [Habbit(name: "Walk the dog", description: "Go on a walk with the dog every day"), Habbit(name: "Do Laundry", description: "Do the dirty laundry")]
     
     
     var body: some View {
         NavigationStack{
             VStack(alignment:.leading){
                 Text("My daily habbits").font(.headline)
-                List(habbits, id: \.name ) { habbit in
+                List(userHabbits.habbitList, id: \.name ) { habbit in
                     Text(habbit.name)
                 }
                 Text("My weekly habbits").font(.headline)
